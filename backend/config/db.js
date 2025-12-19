319b3e1a161e0d9b98bc84e06d9fe5272f398aab
kishan2006-hub <kp0993325@gmail.com>
2025-12-19 15:22:02 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(
-      "mongodb+srv://admin:MyStrongPass123@cluster0.vnqnjvu.mongodb.net/authDB?retryWrites=true&w=majority&appName=Cluster0"
-    );
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error(error.message);
